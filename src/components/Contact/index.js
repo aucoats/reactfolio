332e3939b84aa011d7317271f001c3935abf5c2a
Poke Code <90790144+aucoats@users.noreply.github.com>
2022-03-05 16:18:43 -0500

    basic contact form

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -2,7 +2,32 @@ import React from 'react';
 
 function Contact() {
     return(
-        <p>Contact form</p>
+        <main className="contact">
+            <form>
+                <div className="form-component">
+                    <div>
+                        <label for="name">Name: </label>
+                        <input type="text" placeholder='Name goes here!'></input>
+                    </div>
+                    <div>
+                        <label for="email">Email: </label>
+                        <input type="email" placeholder='Email goes here!'></input>
+                    </div>   
+                    <div className='message-form'>
+                        <label for="message">Leave a message: </label>
+                        <textarea 
+                            placeholder='Have anything to say?'
+                            maxLength={180}></textarea>
+                    </div>
+                </div>
+                <div className="contact-buttons">
+                    <button type="submit">Submit</button>
+                    <a href='mailto:austincoats72@gmail.com'>
+                        <button>Email me!</button>    
+                    </a>
+                </div>
+            </form>
+        </main>
     )
 }
 
