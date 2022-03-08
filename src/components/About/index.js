ef3fb45b6b7d9cfc87e7f21b86a7eda090af9917
Poke Code <90790144+aucoats@users.noreply.github.com>
2022-03-07 20:02:49 -0500

    styling, media queries, about update photo

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -4,13 +4,12 @@ import React from 'react';
 function About() {
     return( 
         <section className="about-me">
-                <img src={require(`../../assets/img/avatar.png`)}>
+                <img src={require(`../../assets/img/avatar.jpg`)}>
                 </img>
                 <p>
-                    Hello! My name is Austin. I am currently working as an industrial hemp retail specialist,
-                    but I have put this portfolio together to showcase my web development prowess for your viewing 
-                    pleasure and professional consideration. Consider utilizing the links in the Projects page to view
-                    some of my works in greater detail. Otherwise, enjoy the portfolio page! This page was built using React. 
+                    Hi, I'm Austin. Perched next to me there is Kashi. We both like taking long walks on a nice trail, but only one of us is a full-stack web developer (it would probably be Kashi if it weren't for the thumb situation). 
+                    I like to build clean, bright, and innovative pages. I love a good challenge but also love to balance work by pursuing interesting goals and projects. My current goal is to be employed as a web developer. 
+                    If my portfolio looks interesting, check out the project page to see some of the cool stuff I've built! Thanks for visiting and feel free to check the contact page! 
                 </p>
         </section>
     )
