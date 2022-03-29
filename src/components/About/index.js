b2d9aa19c919c916ca35b74c2d80ab8944b0bf97
Poke Code <90790144+aucoats@users.noreply.github.com>
2022-03-29 15:08:28 -0400

    about me

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -9,8 +9,9 @@ function About() {
                 </img>
                 <p>
                     Hi, I'm Austin. Perched next to me there is Kashi. We both like taking long walks on a nice trail, but only one of us is a full-stack web developer (it would probably be Kashi if it weren't for the thumb situation). 
-                    I like to build clean, bright, and innovative pages. I love a good challenge but also love to balance work by pursuing interesting goals and projects. My current goal is to be employed as a web developer. 
-                    If my portfolio looks interesting, check out the project page to see some of the cool stuff I've built! Thanks for visiting and feel free to check the contact page! 
+                    I am proficient with both front end and back end development. While models, queries, and data management are always a welcome challenge, I also like to build clean, bright, and innovative pages. Creating models with MongoDB or queries with GraphQL can both
+                    be just as satisfying as perfecting a CSS transition or utilizing a new and interesting React component library.
+                    If my portfolio looks interesting, check out the project page to see some of the cool stuff I've built! If you or someone you know are looking for a developer or find my skillset intriguing, check out the footer or the contact page to contact me! Currently seeking a professional opportunity as a web developer. 
                 </p>
         </section>
     )
