e5debf5f395b20f54488296cf9456f90d3e5afc8
Poke Code <90790144+aucoats@users.noreply.github.com>
2022-03-31 14:28:44 -0400

    updated resume, header, about, some styles and animations

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -8,7 +8,7 @@ function About() {
                     alt="Austin and his pup Kashi near a river">
                 </img>
                 <p>
-                    Hi, I'm Austin. Perched next to me there is Kashi. We both like taking long walks on a nice trail, but only one of us is a full-stack web developer (it would probably be Kashi if it weren't for the thumb situation). 
+                    Hi, I'm Austin. Perched next to me there is Kashi. We both like taking long walks on a nice trail, but only one of us is a full-stack web developer (it would probably be Kashi if it weren't for the thumb situation). I have a BA in Psychology and a full stack web development certificate from the University of North Carolina at Chapel Hill. 
                     I am proficient with both front end and back end development. While models, queries, and data management are always a welcome challenge, I also like to build clean, bright, and innovative pages. Creating models with MongoDB or queries with GraphQL can both
                     be just as satisfying as perfecting a CSS transition or utilizing a new and interesting React component library.
                     If my portfolio looks interesting, check out the project page to see some of the cool stuff I've built! If you or someone you know are looking for a developer or find my skillset intriguing, check out the footer or the contact page to contact me! Currently seeking a professional opportunity as a web developer. 
--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,4 +1,4 @@
-import React from 'react'; 
+import React, { useState } from 'react'; 
 import Navigation from '../Navigation'; 
 
 function Header(props) {
@@ -8,10 +8,15 @@ function Header(props) {
         setCurrentPage
     } = props; 
 
+    const [fullName, setFullName] = useState('Austin');
+
+
     return( 
         <>
             <header>
-                <h2>Austin Coats</h2>
+                <h2 onMouseEnter={() => setFullName('Austin Coats')}
+                    onMouseLeave={() => setFullName('Austin')}>Hi, I'm <span>{fullName}</span>.</h2>
+                <h3>I'm a <span>web developer</span>.</h3>
             <Navigation page={page}
                 currentPage={currentPage}
                 setCurrentPage={setCurrentPage}
